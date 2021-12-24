fe5b160e446a03fb8c1f5cfdf9efb4585435cca2
DaviDev01 <davimendesdafonseca@outlook.com>
2021-12-23 21:17:47 -0300

    link fix

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
             <Link to="/">
                 <h1 className="nav--titulo" >PhotoStore</h1>
             </Link>
-            <Link to="/Carrinho">
+            <Link to="/carrinho">
                 <i className="fas fa-shopping-cart"></i>
             </Link>
         </nav>
